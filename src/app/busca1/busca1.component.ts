c474412ccb0821764a7357c798b2ecb62cb94a4d
nadia brandao silva <nadiabrandao98@gmail.com>
2025-05-01 15:26:46 -0300

    Primeiro commit

---
+++ src/app/busca1/busca1.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BuscarService } from '../buscar.service';
+import { HttpClientModule } from '@angular/common/http'; // ✅ Importando HttpClientModule
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-busca1',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule], // ✅ Adicionando HttpClientModule
+  templateUrl: './busca1.component.html',
+  styleUrls: ['./busca1.component.scss'],
+  providers: [BuscarService]
+})
+export class Busca1Component {
+  buscaForm: FormGroup;
+  resultados: any[] = [];
+
+  constructor(private buscarService: BuscarService, private fb: FormBuilder) {
+    this.buscaForm = this.fb.group({
+      serviceType: [''],
+      state: [''],
+      city: [''],
+      neighborhood: [''],
+      rating: ['']
+    });
+  }
+
+  onBuscar() {
+    console.log('🔍 Botão buscar foi clicado!');
+
+    const filtros = this.buscaForm.value;
+    
+    this.buscarService.buscarLocal(filtros).subscribe(
+      (response) => {
+        console.log('✅ Resposta do servidor:', response);
+
+        //alterei aqui
+        //Ordenando por rating
+        response.results.sort((a: { name: string, rating: number }, b: { name: string, rating: number }) =>         
+          b.rating - a.rating
+        );
+
+        
+
+        this.resultados = response.results; 
+      },
+      (error) => {
+        console.error('❌ Erro na busca:', error);
+      }
+    );
+}
+
+}
+

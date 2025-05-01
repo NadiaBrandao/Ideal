7e9c4f1b3c08ff568d6a0d4780178f7a55ac7af1
nadia brandao silva <nadiabrandao98@gmail.com>
2025-05-01 20:28:00 -0300

    alterações no menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
  import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { Busca1Component } from './busca1/busca1.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-    },
-    { path: 'cadastro', component: CadastroComponent }
+    { path: '', component: HomeComponent},
+    { path: 'cadastro', component: CadastroComponent },
+    { path: 'busca', component: Busca1Component }
 ];
--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from "../formulario/formulario.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [FormularioComponent],
+  imports: [FormularioComponent, NgOptimizedImage],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
